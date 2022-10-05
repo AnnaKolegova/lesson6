b72555a34a766a168c74656a301b0e7f9656b35d
Anna <eekolegov@mail.ru>
2022-10-05 21:09:40 +0300

    Zadacha41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,43 @@
+﻿void Zadacha41()
+{
+    Random random = new Random();
+    int size=random.Next(4,11);
+    Console.WriteLine ($"Размер массива {size}");
+    int[] numbers= new int[size];
+    FillArray(numbers);
+    PrintArray(numbers);
+  
+    Console.WriteLine ($"Положительных элементов {Count(numbers)}");
+}   
+
+    
+void FillArray(int[]num)
+{
+    Console.WriteLine ($"Введите {num.Length} чисел");
+    for (int i=0; i<num.Length;i++)
+    {        
+        num[i]=Convert.ToInt32(Console.ReadLine());
+    }
+  
+}
+void PrintArray(int[]num)
+{
+    for (int i=0; i<num.Length; i++)
+    {
+        Console.Write(num[i]+" ");
+        
+    }
+    Console.WriteLine();
+}
+int Count(int [] num)
+{
+    int count=0;
+    for (int i=0; i<num.Length; i++)
+    {
+        if (num[i]>0)
+        count+=1;
+    }
+return count;
+}
+
+Zadacha41();
