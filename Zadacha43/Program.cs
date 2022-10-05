4ba08bf84e4366faca9e1065d574626c2f7d2b6d
Anna <eekolegov@mail.ru>
2022-10-05 22:04:43 +0300

    Zadacha43

---
+++ Zadacha43/Program.cs
@@ -0,0 +1,13 @@
+﻿void Zadacha43()
+{
+Console.WriteLine("Введите угловой коэффициент первой прямой");
+double k1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки пересечения первой прямой с осью OY");
+double b1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите угловой коэффициент второй прямой");
+double k2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y точки пересечения второй прямой с осью OY");
+double b2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Точка пересечения прямых ({Math.Round((b2-b1)/(k1-k2),1)};{Math.Round(k1*(b2-b1)/(k1-k2)+b1,1)})");
+}
+Zadacha43();
